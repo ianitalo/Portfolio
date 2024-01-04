10767feba030360947b62bbd1638f2d03053520c
ianitalo <ianitalo56@hotmail.com>
2024-01-04 20:39:57 +0000

    Added CV

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,15 +8,26 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Ian</h1>
-        <p className={styles.description}>         
-          I'm a software engineer adept at crafting quality, efficient, and user-centric solutions.
-          Proficient in C#, .NET, React and Azure, I excel in Scrum and DevOps, optimizing APIs for seamless integration.
-          My commitment to clean code, workshops,
-          and problem-solving reflects my dedication to enhancing user experience and driving quality software.
+        <p className={styles.description}>
+          I'm a software engineer adept at crafting quality, efficient, and
+          user-centric solutions. Proficient in C#, .NET, React and Azure, I
+          excel in Scrum and DevOps, optimizing APIs for seamless integration.
+          My commitment to clean code, workshops, and problem-solving reflects
+          my dedication to enhancing user experience and driving quality
+          software.
         </p>
-        <a href="mailto:ianitaloeng@gmail.com" className={styles.contactBtn}>
-          Contact Me
-        </a>
+        <div className={styles.btns}>
+          <a href="mailto:ianitaloeng@gmail.com" className={styles.contactBtn}>
+            Contact Me
+          </a>
+          <a
+            href={getImageUrl("Ian_Gomes_Resume_Updated.pdf")}
+            className={styles.contactBtn}
+            download="Ian_Gomes_Resume_Updated.pdf"
+          >
+            Download CV
+          </a>
+        </div>
       </div>
       <img
         src={getImageUrl("hero/heroImage.png")}
